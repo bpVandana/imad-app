67f7d5b177bf8ceff7bc5c9eae253ddf021ffd8d
bpVandana <vandanakumari291@gmail.com>
2017-08-13 22:03:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,4 @@
-console.log('Loaded!');
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft+5;
-    img.style.marginLeft=marginLeft +'px';
-    
-    
-}
-img.onclick=function()
-{var interval=setInterval(moveRight,50);
-    
-};
+
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function()
