bfdef362e08d1681410945afbab31b2c75d72e2d
bpVandana <vandanakumari291@gmail.com>
2017-08-13 15:34:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get('/:articleName' ,function (req, res)
     { var articleName=req.params.articleName;
-        res.send(articles[articleName]);
+        res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/madi.png', function (req, res) {
