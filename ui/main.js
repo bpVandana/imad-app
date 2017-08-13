e71fcfc7cee6b720629231982cf646712782bc98
bpVandana <vandanakumari291@gmail.com>
2017-08-13 22:23:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,28 @@
-var counter=0;
+
 var button=document.getElementById('counter');
 
 button.onclick=function()
 {
-    
-    counter=counter+1;
-    var span=document.getElementById('count');
+    var request=new XMLHttpRequest();
+    //make a request
+    request.onreadystatechange=function()
+{
+    if(request.readystate==XMLHttpRequest.DONE)
+    {
+        
+        if(request.status==200)
+        {
+            
+            var counter=request.responseText;
+            var span=document.getElementById('count');
     span.innerHTML=counter.toString();
+        }
+    }
+    
+};
+//make the request
+request.open('GET','http://http://vandanakumari291.imad.hasura-app.io/counter',true);
+request.send(null);
+   //render request
+   
 };
